79ba8d746d1f807419683fd0fae309c732c2f662
Yumina1 <nantainingen@me.com>
2022-06-20 03:12:11 +0900

    ver3

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ const janken = ["✊", "✊", "✊", "✊", "✌️", "✌️", "✌️", "✌�
 
 
 cards.forEach(function (_, i) {
+
     //display own cars
     cards[i].addEventListener('click', function () {
         cards[i].classList.add('hidden');
@@ -32,86 +33,76 @@ cards.forEach(function (_, i) {
         cpuCard.innerHTML = setCardCpu;
         overlay.classList.remove('hidden');
         openCards.classList.remove('hidden');
-        console.log(_.textContent, i);
-
     });
 });
 
 
+let firstClick = false;
 
-let R_Click = function (me_janken) {
-    console.log(me_janken);
+const R_Click = function (me_janken) {
 
-    openCards.addEventListener('click', function () {
-        // const me_janken = '✊';
-        gameCounter.textContent = `GAME${gameCount++}`;
-        overlay.classList.add('hidden');
-        //remove open button
-        openCards.classList.add('hidden');
-        // debugger;
-        //decide cpu card
-        const janken2 = Math.trunc(Math.random() * janken.length - 1);
-        //win or lose 
-        if (janken[janken2] === me_janken) {
-            result = 'DRAW';
-        } else if (me_janken === "✊" && janken[janken2] === "✌️" || me_janken === "✌️" && janken[janken2] === "🖐" || me_janken === "🖐" && janken[janken2] === "✊") {
-            result = 'WIN ⭐️+1';
-            currentStar = currentStar += 1;
-            currentStarCpu = currentStarCpu -= 1;
+
+    gameCounter.textContent = `GAME${gameCount++}`;
+    const janken2 = Math.trunc(Math.random() * janken.length - 1);
+
+    //win or lose 
+    if (janken[janken2] === me_janken) {
+        result = 'DRAW';
+    } else if (me_janken === "✊" && janken[janken2] === "✌️" || me_janken === "✌️" && janken[janken2] === "🖐" || me_janken === "🖐" && janken[janken2] === "✊") {
+        result = 'WIN ⭐️+1';
+        currentStar = currentStar += 1;
+        currentStarCpu = currentStarCpu -= 1;
+    } else {
+        result = 'LOSE ⭐️-1'
+        currentStar = currentStar -= 1;
+        currentStarCpu = currentStarCpu += 1;
+    };
+
+    //teke out array value
+    janken.splice(janken2, 1);
+
+    if (!firstClick) {
+
+        openCards.addEventListener('click', function () {
+
+            overlay.classList.add('hidden');
+            //remove open button
+            openCards.classList.add('hidden');
+            //display result
+            console.log(currentStar, currentStarCpu);
+            let gcp = '';
+            if (janken[janken2] === "✊") gcp = 'G';
+            if (janken[janken2] === "✌️") gcp = 'C';
+            if (janken[janken2] === "🖐") gcp = 'P';
+            const displayCpuCard = ` <div class ="display_cpucard">${gcp}<br>${janken[janken2]}</div>`;
+
+            let mygcp = '';
+            if (me_janken === "✊") mygcp = 'G';
+            if (me_janken === "✌️") mygcp = 'C';
+            if (me_janken === "🖐") mygcp = 'P';
+
+            const displayMyCard = ` <div class ="display_mycard">${mygcp}<br>${me_janken}</div>`;
+
+            myCard.innerHTML = displayMyCard;
+            cpuCard.innerHTML = displayCpuCard;
+            displayResult.textContent = result;
+
+            star.textContent = `⭐️${currentStar}`;
+            starCpu.textContent = `⭐️${currentStarCpu}`;
+
             if (currentStarCpu <= 0) {
                 gameclear.classList.remove('hidden');
                 overlay.classList.remove('hidden');
             };
-            console.log('win', currentStar, currentStarCpu);
-            star.textContent = `⭐️${currentStar}`;
-            starCpu.textContent = `⭐️${currentStarCpu}`;
-        } else {
-            result = 'LOSE ⭐️-1'
-            currentStar = currentStar -= 1;
             if (currentStar <= 0) {
                 gameover.classList.remove('hidden');
                 overlay.classList.remove('hidden');
             };
-            currentStarCpu = currentStarCpu += 1;
-            console.log('lose', currentStar, currentStarCpu);
-            star.textContent = `⭐️${currentStar}`;
-            starCpu.textContent = `⭐️${currentStarCpu}`;
-        };
-        //display result
-        console.log(currentStar, currentStarCpu);
-        let gcp = '';
-        if (janken[janken2] === "✊") gcp = 'G';
-        if (janken[janken2] === "✌️") gcp = 'C';
-        if (janken[janken2] === "🖐") gcp = 'P';
 
-        const displayCpuCard = ` <div class ="display_cpucard">
-          ${gcp}<br>${janken[janken2]}
-        </div>`;
 
-        let mygcp = '';
-        if (me_janken === "✊") mygcp = 'G';
-        if (me_janken === "✌️") mygcp = 'C';
-        if (me_janken === "🖐") mygcp = 'P';
-
-        const displayMyCard = ` <div class ="display_mycard">
-        ${mygcp}<br>${me_janken}
-      </div>`;
-
-        myCard.innerHTML = displayMyCard;
-        cpuCard.innerHTML = displayCpuCard;
-        displayResult.textContent = result;
-        //teke out array value
-        janken.splice(janken2, 1);
-    });
+        });
+    }
+    firstClick = true;
 };
-
-
-
-
-
-
-
-
-
 
 
